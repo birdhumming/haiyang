e256011e61c454606fb38401748b59257b68015a
rsdata <rsdata@rsdatas-MacBook-Pro.local>
2020-09-05 23:04:44 -0400

    not

--- codeforces/p.cc
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <cmath>
-using namespace std;
-
-int main(){
-
-cout<<2**3<<endl;
-
-}
--- codeforces/stone.py
+++
@@ -1,2 +0,0 @@
-n,k=map(int,input().split())
-print('1' if(n%(k+1)!=0) else '2')
