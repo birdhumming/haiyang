7ad1d651e8d6da382963e31e61e0cc0ae4dd27b6
rsdata <rsdata@rsdatas-MacBook-Pro.local>
2020-11-06 08:22:17 -0500

    add

--- ML/DeepLearningPython35/nn.py
+++ ML/DeepLearningPython35/nn.py
@@ -6,7 +6,15 @@ import numpy as np
 def sigmoid(z):
 	return 1.0/(1.0+np.exp(-z))
 
+def dsigmoid(z):
+	return sigmoid(z)*(1-sigmoid(z))
+def sigmoid_prime(z):
+	return sigmoid(z)*(1-sigmoid(z))
+
 def relu(z):
 	# retrn np.max(0,z);
 	return np.clip(z,0,np.inf)
-
+def relu_prime(z):
+	return (z>0).astype(int)
+def drelu(z):
+	return (z>0).astype(int)
